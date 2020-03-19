29cdd80d2448997e7fd308805eff6dd8209c988b
PetarPetrovic98 <38016771+PetarPetrovic98@users.noreply.github.com>
2020-03-19 15:32:05 +0100

    added button in About us class

--- app/src/main/java/com/emil/projectgps/AboutUsActivity.java
+++ app/src/main/java/com/emil/projectgps/AboutUsActivity.java
@@ -2,11 +2,15 @@ package com.emil.projectgps;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class AboutUsActivity extends AppCompatActivity {
     TextView textViewAboutUs;
+    Button back;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,6 +18,15 @@ public class AboutUsActivity extends AppCompatActivity {
         setContentView(R.layout.activity_about_us);
 
         textViewAboutUs=(TextView)findViewById(R.id.textAboutUs);
+        back=(Button)findViewById(R.id.btnBack);
+
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(),MapsActivity.class));
+            }
+        });
+        
         textViewAboutUs.setText("This application was created by five students at HKR (Högskolan Kristianstad)." +
                 "The idea of this this app is a mix of two applications, Snapchat/Google Maps, where the user " +
                 "will be able to view the map and chat with friends. There will be a menu where you can " +
